87d3e91ab653a1752434234cb2b1d47dfb4e144f
akashpg-M <waybooster.pg@gmail.com>
2025-02-23 19:17:35 +0530

    Implementing Admin Routes

---
+++ backend/routes/analytics.route.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+import{ adminRoute, protectRoute} from "../middleware/auth.middleware.js";
+import {getAnalyticsData} from "../controller/analytics.controller.js";
+
+const router = express();
+
+router.get("/", protectRoute, adminRoute, async(req, res) => {
+  try{    
+    
+    const analyticsData = await getAnalyticsData();
+
+    const endDate = new Date();
+    const startDate = new Date(endDate.getTime() - 7*24*60*60*1000);
+
+    const dailySalesData = await getAnalyticsData(startDate, endDate);
+ 
+    req.json({
+      analyticsData,
+      dailySalesData,
+    })
+  }catch(error){
+      console.log("Error in analytics route", error.message);
+      res.status(500).json({message: "Server Errror", error: error.message});
+  }
+})
+
+export default router;
